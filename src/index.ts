da278e8a8076e319d5be705cd08f2f63f1ac768a
wuxh <wxh1220@gmail.com>
2024-06-24 14:38:42 +0800

    chore: support DEBUG environment variables to enable debugging

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ async function run(args: Argv = argv) {
 
   main(args, {
     logger: new Logger({
-      isDebug: args.debug,
+      isDebug: args.debug || process.env.DEBUG === command,
     })
   });
 }
